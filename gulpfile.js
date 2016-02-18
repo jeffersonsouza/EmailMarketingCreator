1c66a123e892f4ea21de283e7a654440cdde3d74
Jefferson Souza <hsinfo@gmail.com>
2016-02-18 08:56:29 -0200

    - Fixed the "callback called too many times" bug - Removed unused packages - added support to --port=xxxx parameter in server start

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,34 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     size = require('gulp-size'),
     gls = require('gulp-live-server'),
+    gutil = require('gulp-util'),
+    del = require('del'),
     imagemin = require('gulp-imagemin');
 
 // use gulp --prod
 var env = (gutil.env.prod  === true) ? 'prod' : 'dev';
 
+// use gulp --port=xxx
+var port = (gutil.env.port  !== undefined) ? gutil.env.port : 3000;
+
 // Start a livereload server in 3000 port with dist content
 // and watching templates folder
 gulp.task('serve', ['clean', 'inline'], function() {
 
-    var server = gls.static('dist', 3000);
+    var server = gls.static('dist', port);
     server.start();
 
     //use gulp.watch to trigger server actions(notify, start or stop)
-    gulp.watch(['templates/**/*.html'], function (file) {
+    gulp.watch('templates/**/*.html', ['inline']);
+
+    gulp.watch(['dist/**/*.html'], function (file) {
         server.notify.apply(server, [file]);
     });
-
-    gulp.watch('templates/**/*.html', ['inline']);
 });
 
 // Just watch templates folder
 gulp.task('watch', function(done){
-    gulp.watch('templates/**/*.html', ['inline'])
+    gulp.watch('templates/**/*.html', ['inline']);
 });
 
 // Clean dist folder
@@ -45,6 +50,7 @@ gulp.task('build', ['clean'], function(done){
 // Get css files and style tag content and put inline
 gulp.task('inline', function(done){
     return gulp.src('templates/**/*.html')
+        .pipe(plumber())
         .pipe(inlineCss({
 	        	applyStyleTags: true,
 	        	applyLinkTags: true,
@@ -52,6 +58,5 @@ gulp.task('inline', function(done){
 	        	removeLinkTags: true
         }))
         .pipe(size())
-        .pipe(gulp.dest('dist/'))
-        .on('end', done);
+        .pipe(gulp.dest('dist/'));
 });
